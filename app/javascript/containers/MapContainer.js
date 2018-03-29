ae83e3da88149af586f7c7866f2659a100c5baf9
robert haynes <roberthaynes@roberts-MacBook.local>
2018-03-29 13:16:01 -0400

    added map

---
+++ app/javascript/containers/MapContainer.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import ReactMapboxGl, { Layer, Feature } from 'react-mapbox-gl';
+
+
+const Map = ReactMapboxGl({
+  accessToken: "pk.eyJ1IjoicmFoYXluZXM4MCIsImEiOiJjamZjb3k5bDExdm91NGFxaGJjaDhwcnEwIn0.d-dxgfRiiGePPuS99dHBPg"
+});
+
+class MapContainer extends React.Component {
+  constructor(props) {
+    super(props);
+
+  }
+
+
+  render() {
+
+    return(
+      <div>
+        <h6>Use the map below to find food trucks</h6>
+        <Map
+          center={[-75.163685, 39.952345]}
+          zoom={[12]}
+          style="mapbox://styles/mapbox/streets-v9"
+          containerStyle={{
+            height: "400px",
+            width: "75%"
+          }}>
+          <Layer
+            type="symbol"
+            id="marker"
+            layout={{ "icon-image": "marker-15" }}>
+          </Layer>
+        </Map>
+      </div>
+    )
+  }
+}
+export default MapContainer;
---
+++ spec/javascript/containers/MapContainerSpec.js
@@ -0,0 +1,22 @@
+// import MapContainer from '../../../app/javascript/containers/MapContainer';
+//
+// describe('MapContainer', () => {
+//   let wrapper;
+//
+//   beforeEach(() => {
+//     wrapper = (<MapContainer />)
+//   })
+//
+//   it('should display an h2', () => {
+//     expect(wrapper.find('h2')).toBePresent();
+//   })
+//
+//   it('should display an h6', () => {
+//     wrapper = shallow(<MapContainer />);
+//     expect(wrapper.find('h6')).toBePresent();
+//   })
+//
+//   it('should display an h2', () => {
+//     expect(wrapper.contains(<div className="mapboxgl-map" />)).to.equal(true);
+//   })
+// })
