708346f397811e0dba4bc30585db33c782993780
robert haynes <roberthaynes@roberts-MacBook.local>
2018-04-02 15:04:54 -0400

    add vendor approval

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   devise_for :users
 
   resources :users, only: [:index, :show, :destroy, :edit, :update]
+  put 'accept/:id', to: 'users#accept', as: :accept_user
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
